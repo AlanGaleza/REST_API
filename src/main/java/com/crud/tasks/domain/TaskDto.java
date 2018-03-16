9514aa119de8228b3d068a5628b47ffb0c2aca62
AlanGaleza <alano.gal@gmail.com>
2018-03-16 19:26:12 +0100

    zad. 15.2 getTask

--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -2,11 +2,25 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class TaskDto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column
     private String title;
+
+    @Column
     private  String content;
 }
