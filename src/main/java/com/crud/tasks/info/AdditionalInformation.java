4be4848d251c00251d06c74378ba93078b936a0d
“AlanGaleza <alano.gal@gmail.com>
2018-05-19 12:56:17 +0200

    mailCreatorContext 20.2

--- src/main/java/com/crud/tasks/info/AdditionalInformation.java
+++ src/main/java/com/crud/tasks/info/AdditionalInformation.java
@@ -4,19 +4,36 @@ import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
 public class AdditionalInformation implements InfoContributor {
+    Map<String, String> companyData = new HashMap<>();
     @Override
     public void contribute(Info.Builder builder) {
         //COMPANY INFO
-        Map<String, String> companyData = new HashMap<>();
         companyData.put("phone", "+48 505344961");
         companyData.put("email", "alanGaleza@gmail.com");
         companyData.put("goal", "Our goal is to make the world a better place!");
         companyData.put("name", "TaskCrudAppCreator");
         builder.withDetail("company", companyData);
+    }
+
+    public Map<String, String> companyInfo() {
+        companyData.put("phone", "+48 505344961");
+        companyData.put("email", "alanGaleza@gmail.com");
+        companyData.put("goal", "Our goal is to make the world a better place!");
+        companyData.put("name", "TaskCrudAppCreator");
+        return new HashMap<>(companyData);
+    }
+
+    @Override
+    public String toString() {
+        return companyData.entrySet().stream()
+                .map(entry -> entry.getKey() + " : " + entry.getValue() + "\n")
+                .collect(Collectors.joining());
     }
 }
--- src/main/java/com/crud/tasks/service/TrelloService.java
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -34,7 +34,7 @@ public class TrelloService {
     public CreatedTrelloCardDto createdTrelloCard(final TrelloCardDto trelloCardDto) {
         CreatedTrelloCardDto newCard = trelloClient.createNewCard(trelloCardDto);
         ofNullable(newCard).ifPresent(card -> emailService.send(new Mail(adminConfig.getAdminMail(), SUBJECT,
-                "New Card" + trelloCardDto.getName() + " has been created on your Trello account")));
+                "New Card: " + trelloCardDto.getName() + " has been created on your Trello account")));
         return newCard;
     }
 }
