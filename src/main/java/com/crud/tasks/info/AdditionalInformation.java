d979b95ea129e832d4d061aa95e0af09f8f272d6
“AlanGaleza <alano.gal@gmail.com>
2018-05-19 14:35:15 +0200

    AdditionalInfo repair v2

--- src/main/java/com/crud/tasks/info/AdditionalInformation.java
+++ src/main/java/com/crud/tasks/info/AdditionalInformation.java
@@ -22,11 +22,6 @@ public class AdditionalInformation implements InfoContributor {
     @Override
     public void contribute(Info.Builder builder) {
         //COMPANY INFO
-        Map<String, String> companyData = new HashMap<>();
-        companyData.put("phone", "+48 505344961");
-        companyData.put("email", "alanGaleza@gmail.com");
-        companyData.put("goal", "Our goal is to make the world a better place!");
-        companyData.put("name", "TaskCrudAppCreator");
         builder.withDetail("company", companyData);
     }
 
